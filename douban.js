a36b57ae6ee5e29cb77507b229fda47e0b1f0ddb
Jackie <554586805@qq.com>
2019-02-25 20:23:06 +0800

    near finish

--- douban.js
+++ douban.js
@@ -96,7 +96,7 @@ function renderUs(data){
     var html= '<div class="item"><div class="imagebox"><img src="#" alt=""></div><div class="descript"><h2></h2><p class="type">1994/犯罪、剧情</p><p class="director">导演：弗兰克·德拉邦特</p><p class="actor">主演：蒂姆·罗宾斯、摩根·弗里曼、鲍勃·冈顿</p></div></div>';
     var $node=$(html);
     $node.find('img').attr('src',movie.subject.images.small);
-    $node.find('.descript h2').text(movie.title);
+    $node.find('.descript h2').text(movie.subject.title);
     $node.find('.director').text('导演：'+movie.subject.directors[0].name);
     function actors(){
       var actorsArr=[];
@@ -121,6 +121,83 @@ function renderUs(data){
 
 
 
+
+
+
+var INDEX=0;
+var whetherload=false;
+function getDataSearch(){
+  if(whetherload){return;}
+  whetherload=true;
+  var keyword=$('input').val();
+  $('.loading').show();
+  $.ajax({
+    url:'https://api.douban.com/v2/movie/search',
+    type:'GET',
+    data:{
+      q:keyword/*重点*/,
+      start:INDEX,
+      count:20
+    },
+    dataType:'jsonp'
+  }).done(function(ret){
+    console.log(ret);
+    renderSearch(ret);
+    INDEX += 20;
+    whetherload=false;
+  }).fail(function(){
+    console.log('error...');
+  }).always(function(){
+    $('.loading').hide();
+  })
+ }
+
+ function renderSearch(data){
+  data.subjects.forEach(function(movie){
+    var html= '<div class="item"><div class="imagebox"><img src="#" alt=""></div><div class="descript"><h2></h2><p class="type">1994/犯罪、剧情</p><p class="director">导演：弗兰克·德拉邦特</p><p class="actor">主演：蒂姆·罗宾斯、摩根·弗里曼、鲍勃·冈顿</p></div></div>';
+    var $node=$(html);
+    $node.find('img').attr('src',movie.images.small);
+    $node.find('.descript h2').text(movie.title);
+    function directorArr(){
+      var directorsArr=[];
+      movie.directors.forEach(function(item){
+        directorsArr.push(item.name);
+      })
+      return directorsArr.join('、')
+    }
+    $node.find('.director').text('导演：'+directorArr());
+    function actors(){
+      var actorsArr=[];
+      movie.casts.forEach(function(item){
+        actorsArr.push(item.name);
+      })
+      return actorsArr.join('、')
+    }
+    $node.find('.actor').text('主演：'+actors());
+    $node.find('.descript .type').text(function(){
+      var typeArr=[];
+      movie.genres.forEach(function(item){
+        typeArr.push(item);
+      })
+      return (movie.year+'/'+typeArr.join('、'))
+    })
+    $('.search-result').append($node); 
+  });
+}
+
+$('#search span').on('click',function(){
+  getDataSearch();
+})
+
+
+$(window).on('scroll',function(){
+  if($('section#search').height()+50 === $(window).height()+$(window).scrollTop()){
+    if(clock){clearTimeout(clock);}
+    var clock=setTimeout(function(){
+      getDataSearch();
+    },5000)
+  }
+})
 
 
 
